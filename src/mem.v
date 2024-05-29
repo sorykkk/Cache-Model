347bb417388ba6df30c4ea42bbf5f75c76e13acd
Sorin Besleaga <soryn.besleaga@gmail.com>
2024-05-29 17:57:19 +0300

    completing mem module

--- src/mem.v
+++ src/mem.v
@@ -2,12 +2,41 @@
 //treb de definit aici operatiile pe memorie
 //Un aray de byte-uri
 module mem #(
-    parameter SIZE = (1<<20); // 1MB
+    parameter DEPTH    = (1<<20),   // 1MB
+    parameter WIDTH    = `BYTE,
+    parameter PA_WIDTH = `PA_WIDTH,
+    parameter INIT     = 0          // if 1, resets all the memory
 )
 (
+    input  wire                clk,
 
+    input  wire [PA_WIDTH-1:0] addr,
+
+    input  wire                rd_en,
+    input  wire                wr_en,
+
+    input  wire [WIDTH-1:0]    wr_data,
+    output wire [WIDTH-1:0]    rd_data
 );
 
-    reg [`BYTE-1:0] MM [:SIZE-1];
+    reg [WIDTH-1:0] MM [0:DEPTH-1];
+    // aici putem sa le hardcodam, sau sa facem ca ram
+
+    integer i;
+    initial begin 
+        if(INIT)
+            for(i = 0; i < DEPTH; i = i+1)
+                MM[i] = {WIDTH{1'b0}};
+    end
+
+    // read from Main Memory
+    always @(posedge clk)
+        if(rd_en)
+            rd_data <= MM[addr];
+
+    // write to main Memory
+    always @(posedge clk)
+        if(wr_en)
+            MM[addr] <= wr_data;
 
 endmodule
