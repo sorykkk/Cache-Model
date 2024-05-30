0ead70de43e8c7ca557efcfc13f170b7eb86e5c5
Sorin Besleaga <soryn.besleaga@gmail.com>
2024-05-30 15:44:05 +0300

    final details of architecture

--- src/cache_data.v
+++ src/cache_data.v
@@ -1,6 +1,6 @@
 /*
 * Sorin Besleaga
-* Last modified: 29.05.24
+* Last modified: 30.05.24
 * Status: unfinished
 */
 
@@ -42,18 +42,16 @@ module cache_data #(
     input  wire                  rd_en,        // 1 if load instr
     input  wire                  wr_en,        // 1 if store instr
     
+    //memory control outputs
+    input  wire [MEM_WIDTH-1:0]  mem_rd_blk,
+    output wire [PA_WIDTH-1:0]   mem_addr,
+    output wire                  mem_rd_en,
+    output wire                  mem_wr_en,    // 1 if writing to memory
+    output wire [MEM_WIDTH-1:0]  mem_wr_blk,   // data from cache to memory
+
     output wire                  hit,          // 1 if hit, 0 if miss
     output wire [WRD_WIDTH-1:0]  word_out,     // data from cache to CPU
     output wire [BYTE-1:0]       byte_out,     // byte that is extracted from word
-    
-    //memory control outputs
-    output wire [PA_WIDTH-1:0]   mem_rd_addr,
-    input  wire [MEM_WIDTH-1:0]  mem_rd_blk,
-
-
-    output wire                  mem_wr_en,    // 1 if writing to memory
-    output wire [PA_WIDTH-1:0]   mem_wr_addr,  // memory write address (address in MM where the dirty block is written)
-    output wire [MEM_WIDTH-1:0]  mem_wr_blk,   // data from cache to memory
 );
 
     // Define all ways
@@ -79,38 +77,45 @@ module cache_data #(
 
 
     // Define internal reg
-    reg                  _hit          = 1'b0;
-    reg [WRD_WIDTH-1:0]  _word_out     = {WRD_WIDTH{1'b0}};
-    reg [BYTE-1:0]       _byte_out     = {BYTE{1'b0}};
-    reg [MEM_WIDTH-1:0]  _mem_wr_data  = {WRD_WIDTH{1'b0}};
-    reg [PA_WIDTH-1:0]   _mem_wr_addr  = {PA_WIDTH{1'b0}};
-    reg                  _mem_wr_en    = 1'b0; 
+    // reg                  _hit          = 1'b0;
+    // reg [WRD_WIDTH-1:0]  _word_out     = {WRD_WIDTH{1'b0}};
+    // reg [BYTE-1:0]       _byte_out     = {BYTE{1'b0}};
+    // reg [MEM_WIDTH-1:0]  _mem_wr_data  = {WRD_WIDTH{1'b0}};
+    // reg [PA_WIDTH-1:0]   _mem_wr_addr  = {PA_WIDTH{1'b0}};
+    // reg                  _mem_wr_en    = 1'b0; 
 
-    assign hit = _hit;
+    // assign hit = _hit;
 
-    assign mem_wr_en    = _mem_wr_en;
-    assign mem_data_out = _mem_data_out;
-    assign mem_wr_addr  = _mem_wr_addr;
-    assign word_out     = _word_out;
-    assign byte_out     = _byte_out;
-    assign mem_wr_data  = _mem_wr_data;
-
+    // assign mem_wr_en    = _mem_wr_en;
+    // assign mem_data_out = _mem_data_out;
+    // assign mem_wr_addr  = _mem_wr_addr;
+    // assign word_out     = _word_out;
+    // assign byte_out     = _byte_out;
+    // assign mem_wr_data  = _mem_wr_data;
 
     cache_fsm cache_cntrl(  .clk        (clk),     
                             .rst_n      (rst_n),
+                            .rd_en      (rd_en),
+                            .wr_en      (wr_en),
+
                             .valid      (valid), 
                             .dirty      (dirty),
                             .tag        (tag),     
                             .data       (data), 
-                            .lru        (lru),     
-                            .word_out   (_word_out),
-                            .byte_out   (_byte_out), 
-                            .hit        (_hit),
-                            .mem_wr_data(_mem_wr_data)
+                            .lru        (lru),  
+
+                            .addr       (addr),
+                            .data_wr    (data_wr),
+
+                            .mem_wr_en  (mem_wr_en),
+                            .mem_rd_en  (mem_rd_en),
+                            .mem_rd_blk (mem_rd_blk),
+                            .mem_wr_blk (mem_wr_blk),
+                            .mem_addr   (mem_addr),
+
+                            .word_out   (word_out),
+                            .byte_out   (byte_out), 
+                            .hit        (hit),
                         );
     
-    
-
-
-
 endmodule
--- src/cache_fsm.v
+++ src/cache_fsm.v
@@ -1,5 +1,8 @@
 `include "macros.v"
 
+// Cannot interrupt synthethizably interrupt for loop 
+// That's why i am using hard-codded if's for each way
+
 module control_unit #(
     parameter BYTE      = `BYTE,
     parameter SIZE      = `SIZE,
@@ -28,16 +31,17 @@ module control_unit #(
 
     input  wire [PA_WIDTH-1:0]   addr,
     input  wire [WRD_WIDTH-1:0]  data_wr,
+
     input  wire [MEM_WIDTH-1:0]  mem_rd_blk,
 
     output wire                  mem_wr_en,
     output wire                  mem_rd_en,
-    output reg [PA_WIDTH-1:0]    mem_wr_addr,
+    output reg [PA_WIDTH-1:0]    mem_addr,
     output reg [BLK_WIDTH]       mem_wr_blk,  
 
     output reg                   hit
     output reg [WRD_WIDTH-1:0]   word_out,
-    output reg [BYTE-1:0]        byte_out,
+    output reg [BYTE-1:0]        byte_out
 );
 
     // state parameters
@@ -46,8 +50,7 @@ module control_unit #(
     localparam RD_MISS    = 3'b010;
     localparam WR_HIT     = 3'b011;
     localparam WR_MISS    = 3'b100;
-    localparam RD_EVICT   = 3'b101;
-    localparam WR_EVICT   = 3'b110;
+    localparam EVICT   = 3'b101;
 
     integer i;
 
@@ -58,6 +61,7 @@ module control_unit #(
 
     // state registers
     reg[2:0] state, next;
+    reg rd_m, wr_m;
 
     //change states
     always @(posedge clk, negedge rst_n) begin 
@@ -73,12 +77,22 @@ module control_unit #(
                 if(~rd_en && ~wr_en)   next = IDLE;
                 else if(rd_en && hit)  next = RD_HIT;
                 else if(wr_en && hit)  next = WR_HIT;
-                else if(rd_en && ~hit) next = RD_EVICT;
-                else if(wr_en && ~hit) next = WR_EVICT;
+                else if(rd_en && ~hit) 
+                begin 
+                                       next = EVICT;
+                                       rd_m = 1'b1;
+                                       wr_m = 1'b0;
+                end
+                else if(wr_en && ~hit)
+                begin 
+                                       next = EVICT;
+                                       rd_m = 1'b0;
+                                       wr_m = 1'b1;
+                end
             end
 
-            WR_EVICT :                 next = WR_MISS;
-            RD_EVICT :                 next = RD_MISS;
+            EVICT : if(wr_m)           next = WR_MISS;
+                    if(rd_m)           next = RD_MISS;
 
             RD_MISS, 
             WR_MISS :                  next = IDLE;
@@ -94,157 +108,226 @@ module control_unit #(
                 mem_rd_en <= 1'b0;
                 word_out <= {WRD_WIDTH{1'bz}};
                 byte_out <= {WRD_WIDTH{1'bz}};
+                wr_m <= 1'b0;
+                rd_m <= 1'b0;
             end
 
             RD_HIT: begin 
-                for(i = 0; i < NWAYS; i = i+1) 
-                begin 
-                    //i_th way
-                    if(valid[i][addr[`INDEX]] && (tag[i][addr[`INDEX]] == addr[`TAG])) 
-                    begin
-                        word_out = data[i][addr[`INDEX]][(addr[`BOFFSET]*WRD_WIDTH)+:WRD_WIDTH];
-                        byte_out = word_out[(addr[`WOFFSET]*BYTE) +: BYTE];
+                if(valid[0][addr[`INDEX]] && (tag[0][addr[`INDEX]] == addr[`TAG])) 
+                begin
+                    word_out = data[0][addr[`INDEX]][(addr[`BOFFSET]*WRD_WIDTH)+:WRD_WIDTH];
+                    byte_out = word_out[(addr[`WOFFSET]*BYTE) +: BYTE];
 
-                        // update age registers
-                        for(j = 0; j < NWAYS; j = j+1)
-                            if((i!=j) && (lru[j][addr[`INDEX]] <= lru[i][addr[`INDEX]]))
-                                lru[j][addr[`INDEX]] <= lru[j][addr[`INDEX]] + 1;
-                        lru[i][addr[`INDEX]] <= 2'b00;
-                        
-                        disable  
-                    end
-                end  
+                    // update age registers
+                    if(lru[1][addr[`INDEX]] <= lru[0][addr[`INDEX]]) lru[1][addr[`INDEX]] <= lru[1][addr[`INDEX]] + 1;
+                    if(lru[2][addr[`INDEX]] <= lru[0][addr[`INDEX]]) lru[2][addr[`INDEX]] <= lru[2][addr[`INDEX]] + 1;
+                    if(lru[3][addr[`INDEX]] <= lru[0][addr[`INDEX]]) lru[3][addr[`INDEX]] <= lru[3][addr[`INDEX]] + 1;
+                    lru[0][addr[`INDEX]] = 2'b00;
+                end
+
+                else if(valid[1][addr[`INDEX]] && (tag[1][addr[`INDEX]] == addr[`TAG])) 
+                begin
+                    word_out = data[1][addr[`INDEX]][(addr[`BOFFSET]*WRD_WIDTH)+:WRD_WIDTH];
+                    byte_out = word_out[(addr[`WOFFSET]*BYTE) +: BYTE];
+
+                    // update age registers
+                    if(lru[0][addr[`INDEX]] <= lru[1][addr[`INDEX]]) lru[0][addr[`INDEX]] <= lru[0][addr[`INDEX]] + 1;
+                    if(lru[2][addr[`INDEX]] <= lru[1][addr[`INDEX]]) lru[2][addr[`INDEX]] <= lru[2][addr[`INDEX]] + 1;
+                    if(lru[3][addr[`INDEX]] <= lru[1][addr[`INDEX]]) lru[3][addr[`INDEX]] <= lru[3][addr[`INDEX]] + 1;
+                    lru[1][addr[`INDEX]] = 2'b00;
+                end
+
+                else if(valid[2][addr[`INDEX]] && (tag[2][addr[`INDEX]] == addr[`TAG])) 
+                begin
+                    word_out = data[2][addr[`INDEX]][(addr[`BOFFSET]*WRD_WIDTH)+:WRD_WIDTH];
+                    byte_out = word_out[(addr[`WOFFSET]*BYTE) +: BYTE];
+
+                    // update age registers
+                    if(lru[1][addr[`INDEX]] <= lru[2][addr[`INDEX]]) lru[1][addr[`INDEX]] <= lru[1][addr[`INDEX]] + 1;
+                    if(lru[0][addr[`INDEX]] <= lru[2][addr[`INDEX]]) lru[0][addr[`INDEX]] <= lru[0][addr[`INDEX]] + 1;
+                    if(lru[3][addr[`INDEX]] <= lru[2][addr[`INDEX]]) lru[3][addr[`INDEX]] <= lru[3][addr[`INDEX]] + 1;
+                    lru[2][addr[`INDEX]] = 2'b00;
+                end
+
+                else if(valid[3][addr[`INDEX]] && (tag[3][addr[`INDEX]] == addr[`TAG])) 
+                begin
+                    word_out = data[3][addr[`INDEX]][(addr[`BOFFSET]*WRD_WIDTH)+:WRD_WIDTH];
+                    byte_out = word_out[(addr[`WOFFSET]*BYTE) +: BYTE];
+
+                    // update age registers
+                    if(lru[1][addr[`INDEX]] <= lru[3][addr[`INDEX]]) lru[1][addr[`INDEX]] <= lru[1][addr[`INDEX]] + 1;
+                    if(lru[2][addr[`INDEX]] <= lru[3][addr[`INDEX]]) lru[2][addr[`INDEX]] <= lru[2][addr[`INDEX]] + 1;
+                    if(lru[0][addr[`INDEX]] <= lru[3][addr[`INDEX]]) lru[0][addr[`INDEX]] <= lru[0][addr[`INDEX]] + 1;
+                    lru[3][addr[`INDEX]] = 2'b00;
+                end
             end
 
             WR_HIT : begin 
-                // write-back
-                for(i = 0; i < NWAYS; i = i+1) 
-                begin 
-                    // i_th way
-                    if(valid[i][addr[`INDEX]] && (tag[i][addr[`INDEX]] == addr[`TAG])) 
-                    begin
-                        // for store instr there is no output/byte word
-                        word_out = {WRD_WIDTH{1'bz}};
-                        byte_out = {BYTE{1'bz}};
-                        // mark as dirty block
-                        dirty[i][addr[`INDEX]] <= 1'b1;
-                        // overwrite data info from cache's block
-                        data[i][addr[`INDEX]][(addr[`BOFFSET]*WRD_WIDTH)+:WRD_WIDTH] <= data_wr;
+                 // i_th way
+                if(valid[0][addr[`INDEX]] && (tag[0][addr[`INDEX]] == addr[`TAG])) 
+                begin
+                    // for store instr there is no output/byte word
+                    word_out = {WRD_WIDTH{1'bz}};
+                    byte_out = {BYTE{1'bz}};
+                    // mark as dirty block
+                    dirty[0][addr[`INDEX]] <= 1'b1;
+                    // overwrite data info from cache's block
+                    data[0][addr[`INDEX]][(addr[`BOFFSET]*WRD_WIDTH)+:WRD_WIDTH] <= data_wr;
 
-                        // update age registers
-                        for(j = 0; j < NWAYS; j = j+1)
-                            if((i!=j) && (lru[j][addr[`INDEX]] <= lru[i][addr[`INDEX]]))
-                                lru[j][addr[`INDEX]] <= lru[j][addr[`INDEX]] + 1;
-                        lru[i][addr[`INDEX]] <= 2'b00;
-                        
-                        disable  
-                    end
+                    // update age registers
+                    if(lru[1][addr[`INDEX]] <= lru[0][addr[`INDEX]]) lru[1][addr[`INDEX]] <= lru[1][addr[`INDEX]] + 1;
+                    if(lru[2][addr[`INDEX]] <= lru[0][addr[`INDEX]]) lru[2][addr[`INDEX]] <= lru[2][addr[`INDEX]] + 1;
+                    if(lru[3][addr[`INDEX]] <= lru[0][addr[`INDEX]]) lru[3][addr[`INDEX]] <= lru[3][addr[`INDEX]] + 1;
+                    lru[0][addr[`INDEX]] = 2'b00;
+                end
+
+                else if(valid[1][addr[`INDEX]] && (tag[1][addr[`INDEX]] == addr[`TAG])) 
+                begin
+                    word_out = {WRD_WIDTH{1'bz}};
+                    byte_out = {BYTE{1'bz}};
+                    dirty[1][addr[`INDEX]] <= 1'b1;
+                    data[1][addr[`INDEX]][(addr[`BOFFSET]*WRD_WIDTH)+:WRD_WIDTH] <= data_wr;
+
+                    // update age registers
+                    if(lru[0][addr[`INDEX]] <= lru[1][addr[`INDEX]]) lru[0][addr[`INDEX]] <= lru[0][addr[`INDEX]] + 1;
+                    if(lru[2][addr[`INDEX]] <= lru[1][addr[`INDEX]]) lru[2][addr[`INDEX]] <= lru[2][addr[`INDEX]] + 1;
+                    if(lru[3][addr[`INDEX]] <= lru[1][addr[`INDEX]]) lru[3][addr[`INDEX]] <= lru[3][addr[`INDEX]] + 1;
+                    lru[1][addr[`INDEX]] = 2'b00;
+                end
+
+                else if(valid[2][addr[`INDEX]] && (tag[2][addr[`INDEX]] == addr[`TAG])) 
+                begin
+                    word_out = {WRD_WIDTH{1'bz}};
+                    byte_out = {BYTE{1'bz}};
+                    dirty[2][addr[`INDEX]] <= 1'b1;
+                    data[2][addr[`INDEX]][(addr[`BOFFSET]*WRD_WIDTH)+:WRD_WIDTH] <= data_wr;
+
+                    // update age registers
+                    if(lru[1][addr[`INDEX]] <= lru[2][addr[`INDEX]]) lru[1][addr[`INDEX]] <= lru[1][addr[`INDEX]] + 1;
+                    if(lru[0][addr[`INDEX]] <= lru[2][addr[`INDEX]]) lru[0][addr[`INDEX]] <= lru[0][addr[`INDEX]] + 1;
+                    if(lru[3][addr[`INDEX]] <= lru[2][addr[`INDEX]]) lru[3][addr[`INDEX]] <= lru[3][addr[`INDEX]] + 1;
+                    lru[2][addr[`INDEX]] = 2'b00;
+                end
+
+                else if(valid[3][addr[`INDEX]] && (tag[3][addr[`INDEX]] == addr[`TAG])) 
+                begin
+                    word_out = {WRD_WIDTH{1'bz}};
+                    byte_out = {BYTE{1'bz}};
+                    dirty[3][addr[`INDEX]] <= 1'b1;
+                    data[3][addr[`INDEX]][(addr[`BOFFSET]*WRD_WIDTH)+:WRD_WIDTH] <= data_wr;
+
+                    // update age registers
+                    if(lru[1][addr[`INDEX]] <= lru[3][addr[`INDEX]]) lru[1][addr[`INDEX]] <= lru[1][addr[`INDEX]] + 1;
+                    if(lru[2][addr[`INDEX]] <= lru[3][addr[`INDEX]]) lru[2][addr[`INDEX]] <= lru[2][addr[`INDEX]] + 1;
+                    if(lru[0][addr[`INDEX]] <= lru[3][addr[`INDEX]]) lru[0][addr[`INDEX]] <= lru[0][addr[`INDEX]] + 1;
+                    lru[3][addr[`INDEX]] = 2'b00;
                 end
             end
 
-            RD_EVICT : begin 
-                for (i = 0; i < NWAYS; i = i + 1) 
+            EVICT : begin 
+                mem_rd_en <= 1'b1;
+                
+                // write-back 
+                if(valid[0][addr[`INDEX]] && (lru[0][addr[`INDEX]] == 2'b11) && (dirty[0][addr[`INDEX]] == 1'b1)) 
                 begin 
-                    if (~valid[i][addr[`INDEX]]) 
-                    begin 
-                        data[i][addr[`INDEX]] <= mem_rd_blk;
-                        tag[i][addr[`INDEX]] <= addr[`TAG];
-                        dirty[i][addr[`INDEX]] <= 1'b0;
-                        valid[i][addr[`INDEX]] <= 1'b1;
-                        disable;
-                    end
+                    mem_addr <= addr;
+                    mem_wr_en <= 1'b1;
+                    mem_wr_blk <= data[0][addr[`INDEX]];
+                end
 
-                    else if((lru[i][addr[`INDEX]] == 2'b11) && (dirty[i][addr[`INDEX]] == 1'b1)) 
-                    begin 
-                        mem_wr_addr <= addr;
-                        mem_wr_en <= 1'b1;
-                        mem_wr_blk <= data[addr[`INDEX]];
-                        disable;
-                    end
+                else if(valid[1][addr[`INDEX]] && (lru[1][addr[`INDEX]] == 2'b11) && (dirty[1][addr[`INDEX]] == 1'b1)) 
+                begin 
+                    mem_addr <= addr;
+                    mem_wr_en <= 1'b1;
+                    mem_wr_blk <= data[1][addr[`INDEX]];
+                end
+
+                else if(valid[2][addr[`INDEX]] && (lru[2][addr[`INDEX]] == 2'b11) && (dirty[2][addr[`INDEX]] == 1'b1)) 
+                begin 
+                    mem_addr <= addr;
+                    mem_wr_en <= 1'b1;
+                    mem_wr_blk <= data[2][addr[`INDEX]];
+                end
+
+                else if(valid[3][addr[`INDEX]] && (lru[3][addr[`INDEX]] == 2'b11) && (dirty[3][addr[`INDEX]] == 1'b1)) 
+                begin 
+                    mem_addr <= addr;
+                    mem_wr_en <= 1'b1;
+                    mem_wr_blk <= data[3][addr[`INDEX]];
                 end
             end
 
-            WR_EVICT : begin 
-
-                for (i = 0; i < NWAYS; i = i + 1) 
-                begin 
-                    if (~valid[i][addr[`INDEX]]) 
-                    begin 
-                        data[i][addr[`INDEX]] <= mem_rd_blk;
-                        tag[i][addr[`INDEX]] <= addr[`TAG];
-                        dirty[i][addr[`INDEX]] <= 1'b0;
-                        valid[i][addr[`INDEX]] <= 1'b1;
-                        disable;
-                    end
-                end
-
-            end
-
-            //aici e pizda
-
-            //AICI TREB DE SCHIMBAT TOT
             RD_MISS: begin 
+                word_out = mem_rd_blk[(addr[`BOFFSET]*WRD_WIDTH)+:WRD_WIDTH];
+                byte_out = word_out[(addr[`WOFFSET]*BYTE) +: BYTE];
 
-                for (i = 0; i < NWAYS; i = i + 1) 
-                begin 
-                    if (~valid[i][addr[`INDEX]]) 
-                    begin 
-                        word_out = mem_rd_blk[(addr[`BOFFSET]*WRD_WIDTH)+:WRD_WIDTH];
-                        byte_out = word_out[(addr[`WOFFSET]*BYTE) +: BYTE];
-
-                        data[i][addr[`INDEX]] <= mem_rd_blk;
-                        tag[i][addr[`INDEX]] <= addr[`TAG];
-                        dirty[i][addr[`INDEX]] <= 1'b0;
-                        valid[i][addr[`INDEX]] <= 1'b1;
-                        disable;
-                    end
+                if(~valid[0][addr[`INDEX]] || (lru[0][addr[`INDEX]] == 2'b11))
+                begin
+                    data[0][addr[`INDEX]] <= mem_rd_blk;
+                    tag[0][addr[`INDEX]] <= addr[`TAG];
+                    dirty[0][addr[`INDEX]] <= 1'b0;
+                    valid[0][addr[`INDEX]] <= 1'b1;
+                end
+                
+                else if(~valid[1][addr[`INDEX]] || (lru[1][addr[`INDEX]] == 2'b11))
+                begin
+                    data[1][addr[`INDEX]] <= mem_rd_blk;
+                    tag[1][addr[`INDEX]] <= addr[`TAG];
+                    dirty[1][addr[`INDEX]] <= 1'b0;
+                    valid[1][addr[`INDEX]] <= 1'b1;
                 end
 
+                else if(~valid[2][addr[`INDEX]] || (lru[2][addr[`INDEX]] == 2'b11))
+                begin
+                    data[2][addr[`INDEX]] <= mem_rd_blk;
+                    tag[2][addr[`INDEX]] <= addr[`TAG];
+                    dirty[2][addr[`INDEX]] <= 1'b0;
+                    valid[2][addr[`INDEX]] <= 1'b1;
+                end
 
-                // for(i = 0; i < NWAYS; i = i + 1) 
-                // begin 
-                //     if (lru[i][addr[`INDEX]] == 2'b11) 
-                //     begin 
-                //         word_out = mem_rd_blk[(addr[`BOFFSET]*WRD_WIDTH)+:WRD_WIDTH];
-                //         byte_out = word_out[(addr[`WOFFSET]*BYTE) +: BYTE];
-
-                //         data[i][addr[`INDEX]] <= mem_rd_blk;
-                //         tag[i][addr[`INDEX]] <= addr[`TAG];
-                //         dirty[i][addr[`INDEX]] <= 1'b0;
-                //         valid[i][addr[`INDEX]] <= 1'b1;
-                //         disable;
-                //     end
-                // end
+                else if(~valid[3][addr[`INDEX]] || (lru[3][addr[`INDEX]] == 2'b11))
+                begin
+                    data[3][addr[`INDEX]] <= mem_rd_blk;
+                    tag[3][addr[`INDEX]] <= addr[`TAG];
+                    dirty[3][addr[`INDEX]] <= 1'b0;
+                    valid[3][addr[`INDEX]] <= 1'b1;
+                end
             end
 
             WR_MISS : begin 
+                word_out = {WRD_WIDTH{1'bz}};
+                byte_out = {BYTE{1'bz}};
 
-                for (i = 0; i < NWAYS; i = i + 1) 
-                begin 
-                    if (~valid[i][addr[`INDEX]]) 
-                    begin 
-                        data[i][addr[`INDEX]] <= mem_rd_blk;
-                        tag[i][addr[`INDEX]] <= addr[`TAG];
-                        dirty[i][addr[`INDEX]] <= 1'b0;
-                        valid[i][addr[`INDEX]] <= 1'b1;
-                        disable;
-                    end
+                if(~valid[0][addr[`INDEX]] || (lru[0][addr[`INDEX]] == 2'b11))
+                begin
+                    data[0][addr[`INDEX]] <= data_wr;
+                    tag[0][addr[`INDEX]] <= addr[`TAG];
+                    dirty[0][addr[`INDEX]] <= 1'b1;
+                    valid[0][addr[`INDEX]] <= 1'b1;
                 end
 
+                else if(~valid[1][addr[`INDEX]] || (lru[1][addr[`INDEX]] == 2'b11))
+                begin
+                    data[1][addr[`INDEX]] <= data_wr;
+                    tag[1][addr[`INDEX]] <= addr[`TAG];
+                    dirty[1][addr[`INDEX]] <= 1'b1;
+                    valid[1][addr[`INDEX]] <= 1'b1;
+                end
 
-                for(i = 0; i < NWAYS; i = i+1)
-                begin 
-                    if(lru[i][addr[`INDEX]] == 2'b11)
-                    begin 
-                        word_out <= {WRD_WIDTH{1'bz}};
-                        byte_out <= {BYTE{1'bz}};
+                else if(~valid[2][addr[`INDEX]] || (lru[2][addr[`INDEX]] == 2'b11))
+                begin
+                    data[2][addr[`INDEX]] <= data_wr;
+                    tag[2][addr[`INDEX]] <= addr[`TAG];
+                    dirty[2][addr[`INDEX]] <= 1'b1;
+                    valid[2][addr[`INDEX]] <= 1'b1;
+                end
 
-                        data[i][addr[`INDEX]] <= mem_rd_blk;
-                        tag[i][addr[`INDEX]] <= addr[`TAG];
-                        dirty[i][addr[`INDEX]] <= 1'b1;
-                        valid[i][addr[`INDEX]] <= 1'b1;
-                        disable;
-                    end
+                else if(~valid[3][addr[`INDEX]] || (lru[3][addr[`INDEX]] == 2'b11))
+                begin
+                    data[3][addr[`INDEX]] <= data_wr;
+                    tag[3][addr[`INDEX]] <= addr[`TAG];
+                    dirty[3][addr[`INDEX]] <= 1'b1;
+                    valid[3][addr[`INDEX]] <= 1'b1;
                 end
             end
         endcase
